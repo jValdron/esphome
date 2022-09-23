7046d6c2d2afc6f96f0e31beaf6d04e03576179a
Jason Valdron <jason@valdron.ca>
2022-09-23 11:10:51 -0300

    Fix for esp8266

--- esphome/components/tmc2209/tmc2209.h
+++ esphome/components/tmc2209/tmc2209.h
@@ -6,6 +6,8 @@
 
 #include <TMCStepper.h>
 
+#define SERIAL_PORT Serial
+
 namespace esphome {
 namespace tmc {
 
@@ -15,7 +17,7 @@ class TMC2209 : public stepper::Stepper, public Component, public uart::UARTDevi
       : step_pin_(step_pin),
         dir_pin_(dir_pin),
         reverse_direction_(reverse_direction),
-        stepper_driver_(this, 0.15f, 0b00) {}
+        stepper_driver_(&SERIAL_PORT, 0.15f, 0b00) {}
 
   void set_sleep_pin(GPIOPin *sleep_pin) { this->sleep_pin_ = sleep_pin; }
   void setup() override;
